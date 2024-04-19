f73b8656f3f95a487f899da02051f1515e2ffcb4
Ngo Quang Duong <96757935+DuongNQ09@users.noreply.github.com>
2024-04-19 00:49:55 +0000

    create project

---
+++ test.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        printNumbersAndCheckEvenOdd();
+    }
+
+    public static void printNumbersAndCheckEvenOdd() {
+        for (int i = 1; i <= 10; i++) { // Vòng lặp từ 1 đến 10
+            if (i % 2 == 0) { // Kiểm tra số chẵn
+                System.out.println(i + " là số chẵn.");
+            } else {
+                System.out.println(i + " là số lẻ.");
+            }
+        }
+    }
+}
